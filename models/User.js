c64d1470191c6d585eef60fa68c7e564e7cb0039
yaa223 <yaa223@lehigh.edu>
2021-11-29 18:50:07 -0500

    Schema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
+  email: { type: String, unique: true },
+  UserType: String,
   password: String,
   balence: Number
 });
