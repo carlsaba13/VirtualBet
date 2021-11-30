6762e98a154e163eb2a5586ce3c77406ca7d8e0f
yaa223 <yaa223@lehigh.edu>
2021-11-30 17:20:47 -0500

    hash across auth and users

--- public/javascripts/authenticate.js
+++ public/javascripts/authenticate.js
@@ -1,3 +1,15 @@
+//https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
+
 async function validateSignIn() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('pass').value;
@@ -5,7 +17,7 @@ async function validateSignIn() {
     let user = await fetch('http://localhost:3000/users/' + email);
     user = await user.json();
 
-    if(user.password === password) {
+    if(user.password === hashCode(password)) {
         document.cookie = "email=" + user.email;
         if(user.bookie)
             window.location.replace("http://localhost:3000/statistics");
