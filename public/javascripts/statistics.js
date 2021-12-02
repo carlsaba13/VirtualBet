9935e50b691d7e836235c1d1cdf23ee45b1bc8e3
rcc223 <rcc223@lehigh.edu>
2021-12-01 19:12:39 -0500

    Co-authored-by: JackMiller44 <JackMiller44@users.noreply.github.com>

--- public/javascripts/statistics.js
+++ public/javascripts/statistics.js
@@ -1,41 +1,82 @@
 async function addStatistics() {
     //Add total number of users
-    /* let users = await fetch('/users/');
+    let users = await fetch('/users/');
     users = await users.json()
-    users = users.body;
 
-    console.log(users); */
+    console.log(users);
 
     const totalUsersDiv = document.getElementById('total-users');
 
     const totalUsersText = document.createElement('h1');
-    //totalUsersText.innerHTML = users.length;
-    totalUsersText.innerHTML = 'Total Users: ' + '56';
-
+    totalUsersText.innerHTML = 'Total Users: ' + users.length;
     totalUsersDiv.appendChild(totalUsersText);
 
     //Add total number of bets
+    let bets = [];
+    console.log(typeof bets);
+
+    for(const user of users) {
+        let betsFetch = await fetch('/bets/' + user.email);
+        betsFetch = await betsFetch.json();
+        betsFetch.forEach(bet => {
+            bets.push(bet);
+        });
+    }
+
+    console.log(bets);
+
     const betsPlacedDiv = document.getElementById('bets-placed');
     const totalBetsText = document.createElement('h1');
-    totalBetsText.innerHTML = 'Total Bets Placed: ' + '145';
+    totalBetsText.innerHTML = 'Total Bets Placed: ' + bets.length;
     betsPlacedDiv.appendChild(totalBetsText);
 
     //Add total number of bets won
+    let betsWon = 0;
+
+    bets.forEach(bet => {
+        if(bet.victory === true)
+            betsWon++;
+    });
+
     const betsWonDiv = document.getElementById('bets-won');
     const totalBetsWonText = document.createElement('h1');
-    totalBetsWonText.innerHTML = 'Total Bets Won: ' + '73';
+    totalBetsWonText.innerHTML = 'Total Bets Won: ' + betsWon;
     betsWonDiv.appendChild(totalBetsWonText);
 
     //Add Net Profit
-    const netProfitDiv = document.getElementById('net-profit');
-    const netProfitText = document.createElement('h1');
-    netProfitText.innerHTML = 'Net Profit: $' + '7';
+    let netProfit = 0
+
+    bets.forEach(bet => {
+        let amt = bet["amount"];
+        let odds = bet["odds"];
+        if(bet)
+            if(odds < 0){
+                odds *= -1;
+                netProfit -= Math.ceil(((odds+100)/odds)*amt) + amt;
+            }
+            else{
+                netProfit -= Math.ceil(((odds+100)/100)*amt) + amt;
+            }        netProfit += 
+    });
+
+    const netProbet["victory"] ;=fitfalse={
+            netProfit += amount
+        }xt = document.createElement('h1');
+    netProfitText.innerHTML = 'Net Profit: $' + netProfit;
     netProfitDiv.appendChild(netProfitText);
 
     //Add Average Bet Amount
+    let avgBetAmount = 0;
+
+    bets.forEach(bet => {
+        avgBetAmount += bet.amount;
+    });
+
+    avgBetAmount /= bets.length;
+
     const avgBetDiv = document.getElementById('avg-bet');
     const avgBetText = document.createElement('h1');
-    avgBetText.innerHTML = 'Average Bet Amount: $' + '45';
+    avgBetText.innerHTML = 'Average Bet Amount: $' + avgBetAmount;
     avgBetDiv.appendChild(avgBetText);
 
     //Add Average Profit Per Bet
@@ -45,7 +86,8 @@ async function addStatistics() {
     avgProfitDiv.appendChild(avgProfitText);
 
 
-}
+    let avgProfit = netProfit / bets.length;
+avgProfit}
 
 //Loads validate function onto week form
 async function init(){
